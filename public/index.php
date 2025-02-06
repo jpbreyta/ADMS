6c17c46a25c6b0005708572f492ce6be9352a0e0
wainoyama <jpreytapogi@gmail.com>
2025-02-06 19:05:06 +0800

    updated

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
 require_once __DIR__ . '/../config/env.php'; 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
+    <title></title>
+</head>
+<body>
+    <header>
+        <nav>
+            <!-- Navigation links go here -->
+        </nav>
+    </header>
+    <main>
+        <div>
+            <!-- Main content goes here -->
+        </div>
+    </main>
+    <footer>
+        <!-- Footer content goes here -->
+    </footer>
+</body>
+</html>
