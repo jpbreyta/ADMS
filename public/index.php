18d197635b74b0b59319b9f153cca0374d221d96
wainoyama <jpreytapogi@gmail.com>
2025-02-16 23:57:18 +0800

    update

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ if (!in_array($page, $allowed_pages)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My PHP Website</title>
+    <title>CRM</title>
     <?php include 'includes/assets.php'; ?>
 
 </head>
